47cb001ba0e8dd214396a1e9dcb25ac546c41a5c
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-11-04 08:27:05 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -15,6 +15,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
+                        InlineKeyboardButton("📱 CONTACT 📱", callback_data = "close")
                         InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", callback_data = "close")
                     ]
                 ]
