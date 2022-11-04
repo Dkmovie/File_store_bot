d6535138c9afbbd9d080765809d9866951c04e52
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-11-04 18:06:52 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -26,4 +26,4 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except:
             pass
-            reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+            reply_markup = InlineKeyboardMarkup(
